197f12f9e8188609e24a31d0fb270d240f433196
IyanSR <ikhyan7@gmail.com>
2019-10-28 23:05:57 +0700

    Add Order By ASC

--- src/Models/novel.js
+++ src/Models/novel.js
@@ -24,6 +24,7 @@ module.exports = {
             return `${field} LIKE ? `;
           })
           .join(' AND ');
+        query += 'ORDER BY b.id ASC';
       } else {
         query += 'ORDER BY b.id ASC';
       }
