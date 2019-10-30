de3eb0af15b8581ec18c08dc8de74945f64fe47a
IyanSR <ikhyan7@gmail.com>
2019-10-30 14:11:58 +0700

    update query

--- src/Helpers/query.js
+++ src/Helpers/query.js
@@ -3,9 +3,9 @@ module.exports = {
     let query = `SELECT b.id, b.title, b.author, b.image_url, s.novel_status AS status, g.genre 
       FROM novels b 
         INNER JOIN genres g 
-          ON b.genre = g.genre_id 
+          ON b.genre = g.id 
         INNER JOIN novel_status s 
-          ON b.status = s.status_id `;
+          ON b.status = s.id `;
 
     return query;
   },
