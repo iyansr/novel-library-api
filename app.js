c6be92ad9eeda54f59598e6a4bc3b1d88c32d897
IyanSR <ikhyan7@gmail.com>
2019-11-10 23:48:10 +0700

    knn

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const logger = require('morgan');
+// const logger = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./src/Routes');
 const cors =  require('cors')
 const app = express();
-const port = 3367 || process.env.PORT;
+const port = process.env.PORT;
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
